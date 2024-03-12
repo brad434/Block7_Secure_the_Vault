dd287a9ca5a215d8b0d9ea7a245c5fdce8498f87
Bradley Chery <Bradley434@hotmail.com>
2024-03-11 21:07:44 -0400

    completed task

---
+++ script.js
@@ -0,0 +1,17 @@
+//this will present the string with the words provided
+console.log(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+);
+
+//creating a variable with a given equation
+var multiple = 5 * 2;
+var subtract = 41 - 1;
+var add = 9 + 1;
+
+//placing all arithmetic operation
+console.log(multiple);
+console.log(subtract);
+console.log(add);
+
+//this step is going to present the popup
+alert("WELCOME !");
